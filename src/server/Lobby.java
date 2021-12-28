ec08b7de0c9e54cdb1a8e1732e8230e4bab32c2b
Carlos Fernandes <carlosffernandes28@gmail.com>
2021-12-28 16:13:54 +0000

    added server package

---
+++ src/server/Lobby.java
@@ -0,0 +1,76 @@
+package server;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Lobby implements Runnable{
+    public static ArrayList<Lobby> lobby = new ArrayList<>();
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String clientUsername;
+
+    public Lobby(Socket socket) {
+        try{
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUsername = bufferedReader.readLine();
+            lobby.add(this);
+            broadcastMessage("SERVER: " + clientUsername + " has entered the chat!");
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    @Override
+    public void run() {
+        String messageFromClient;
+
+        while(socket.isConnected()){
+            try {
+                messageFromClient = bufferedReader.readLine();
+                broadcastMessage(messageFromClient);
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
+    }
+    public void broadcastMessage(String messageToSend){
+        for(Lobby lobby : lobby){
+            try {
+                if(!lobby.clientUsername.equals(clientUsername)) {
+                    lobby.bufferedWriter.write(messageToSend);
+                    lobby.bufferedWriter.newLine();
+                    lobby.bufferedWriter.flush();
+                }
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+        }
+    }
+
+    public void removeClient() {
+        lobby.remove(this);
+        broadcastMessage("SERVER: " + clientUsername + " has left the chat!");
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+        removeClient();
+        try {
+            if(bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if(bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if(socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/server/Server.java
@@ -0,0 +1,42 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+     private ServerSocket serverSocket;
+     public Server(ServerSocket serverSocket) {
+         this.serverSocket = serverSocket;
+     }
+     public void startServer(){
+         try{
+             while(!serverSocket.isClosed()){
+                 Socket socket = serverSocket.accept();
+                 System.out.println("A new client has connected!");
+                 Lobby lobby = new Lobby(socket);
+
+                 Thread thread = new Thread(lobby);
+                 thread.start();
+             }
+         } catch (IOException e) {
+
+         }
+     }
+
+     public void closeServerSocket(){
+         try{
+             if(serverSocket != null){
+                 serverSocket.close();
+             }
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+     }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
+}

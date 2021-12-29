78407f53ff4431c3097059e303902b235ac413eb
GenesisMRL <nunomiglima@gmail.com>
2021-12-29 14:13:32 +0000

    added Team stuff

--- src/game/Team.java
+++ src/game/Team.java
@@ -1,4 +1,47 @@
 package game;
 
+import java.util.ArrayList;
+
 public class Team {
+    private int firewalls;
+    private int viruses;
+    private int antiviruses;
+    private final ArrayList<Player> players;
+
+    public Team(ArrayList<Player> players){
+        this.players = players;
+        this.firewalls = 5 * players.size();
+        this.viruses = 0;
+        this.antiviruses = 0;
+    }
+
+    public void addVirus(int virus){
+        this.viruses = this.viruses + virus;
+    }
+
+    public void addAntivirus(int antivirus){
+        this.antiviruses = this.antiviruses + antivirus;
+    }
+
+    public void updateFirewalls(Team opposingTeam){
+        this.firewalls = this.firewalls - (Math.max((opposingTeam.getViruses() - this.antiviruses),0));
+    }
+
+    /**GETTERS**/
+    public int getFirewalls() {
+        return firewalls;
+    }
+
+    public int getViruses() {
+        return viruses;
+    }
+
+    public int getAntiviruses() {
+        return antiviruses;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
 }

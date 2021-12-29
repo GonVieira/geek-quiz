339b4a9a5b20977aa059f2fdd54de58d1fb75ac9
Nuno Neto <nunoneto2015@gmail.com>
2021-12-29 16:29:34 +0000

    Added first player's features

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,4 +1,42 @@
 package game;
 
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class Player {
+
+    private Socket socket;
+    private int score = 0;
+   // private HashMap<String, Integer> questions;
+
+    private LinkedList answeredQuestions;
+
+
+
+    public void saveQuestion(String question) {
+        answeredQuestions.add(question);
+    }
+
+    public boolean questionAnswered(String question) {
+
+        if (answeredQuestions.contains(question)) {
+            return true;
+        }
+        return false;
+
+    }
+
+    public void checkIfIsCorrect(int answer, Integer correctAnswer) {
+
+        if (answer == correctAnswer) {
+            System.out.println("Correct answer.");
+            score++;
+            System.out.println("Now your score is: " + score);
+
+        } else {
+            System.out.println("Incorrect answer.");
+            System.out.println("Score: " + score);
+        }
+    }
 }
